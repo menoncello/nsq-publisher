862d7bfe07c651c2f10819c3e48072b9831ca7c1
Eduardo Menoncello <eduardo.menoncello@gmail.com>
2017-03-12 11:05:29 -0300

    Promesifying the calls

--- index.js
+++ index.js
@@ -1,98 +1,42 @@
-const nsq = require('nsqjs');
-const request = require('request');
+const Promise = require('bluebird');
+const nsq = Promise.promisifyAll(require('nsqjs'));
+const request = Promise.promisifyAll(require('request'));
 
-function Publisher(options) {
-	this.options = options;
-	this.optionsErrors = [];
-}
-
-Publisher.prototype.createTopic = createTopic;
-Publisher.prototype.publish = publish;
-Publisher.prototype.checkOptions = checkOptions;
-
-module.exports = Publisher;
-
-function createTopic(callback) {
-	if (!this.checkOptions()) {
-		callback({
-			msg: this.optionsErrors.join('\n'),
-		});
-		return;
+class Publisher {
+	constructor(dataUrl, dataPort, topic) {
+		this.dataUrl = dataUrl;
+		this.dataPort = dataPort;
+		this.topic = topic;
 	}
 
-	request.post(
-		`${this.options.dataUrl}:${this.options.dataPort}/topic/create?topic=${this.options.topic}`,
-		topicCreated);
-
-	function topicCreated(err) {
-		if (err) {
-			console.error(err);
-			callback(err);
-			return;
-		}
-
-		callback();
-	}
-}
-
-function publish(message, callback) {
-	if (!this.checkOptions()) {
-		callback({
-			msg: this.optionsErrors.join('\n'),
-		});
-		return;
+	createTopic(callback) {
+		request.postAsync(
+			`${this.dataUrl}:${this.dataPort}/topic/create?topic=${this.topic}`
+			)
+			.catch(callback)
+			.then(() => callback());
 	}
 
-	const nsqWriter = new nsq.Writer(this.options.dataUrl, this.options.dataPort);
+	publish(message, callback) {
 
-	nsqWriter.connect();
-	nsqWriter.on('ready', (err) => {
-		if (err) {
-			callback(err);
-			return;
-		}
+		const nsqWriter = new nsq.Writer(this.options.dataUrl, this.options.dataPort);
 
-		nsqWriter.publish(this.options.topic, message, (errPublish) => {
-			nsqWriter.close();
-			if (errPublish) {
-				callback(errPublish);
+		nsqWriter.connect();
+		nsqWriter.on('ready', ready);
+
+		function ready(err) {
+			if (err) {
+				callback(err);
 				return;
 			}
 
-			callback();
-		});
-	});
+			nsqWriter.publishAsync(this.options.topic, message)
+				.then(nsqWriter.close)
+				.catch(callback)
+				.then(() => callback());
+		}
+	}
 }
 
-function checkOptions() {
-	if (!this.options) {
-		const err = 'options is a required parameter';
-		if (this.optionsErrors.indexOf(err) === -1) {
-			this.optionsErrors.push(err);
-		}
-		return false;
-	}
+module.exports = Publisher;
 
-	if (!this.options.dataUrl) {
-		const err = 'Property of options "dataUrl" is a required';
-		if (this.optionsErrors.indexOf(err) === -1) {
-			this.optionsErrors.push(err);
-		}
-	}
-
-	if (!this.options.dataPort) {
-		const err = 'Property of options "dataPort" is a required';
-		if (this.optionsErrors.indexOf(err) === -1) {
-			this.optionsErrors.push(err);
-		}
-	}
-
-	if (!this.options.topic) {
-		const err = 'Property of options "topic" is a required';
-		if (this.optionsErrors.indexOf(err) === -1) {
-			this.optionsErrors.push(err);
-		}
-	}
-
-	return this.optionsErrors.length === 0;
-}
